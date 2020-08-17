a894930fed6e2fee0187799d615d9247425ac51f
Martin Heidegger <martin.heidegger@gmail.com>
2020-08-17 21:19:44 +0900

    refactor: streamlined toPromise code

--- src/util/isPromiseLike.ts
+++ src/util/isPromiseLike.ts
@@ -1,4 +1,4 @@
 // Adapted from https://github.com/typestack/routing-controllers/blob/4a56d176db77bc081dfcd3d8550e8433b5bc476e/src/util/isPromiseLike.ts#L1-L6
 export function isPromiseLike <T> (arg: any): arg is PromiseLike<T> {
-  return arg !== null && arg !== undefined && typeof arg === 'object' && typeof arg.then === 'function'
+  return typeof arg?.then === 'function'
 }
--- src/util/toPromise.ts
+++ src/util/toPromise.ts
@@ -6,10 +6,10 @@ const cache = new WeakMap<any, Promise<any>>()
 
 // eslint-disable-next-line @typescript-eslint/promise-function-async
 export function toPromise<T> (input: T | PromiseLike<T>): Promise<T> {
+  if (input instanceof Promise) {
+    return input
+  }
   if (isPromiseLike(input)) {
-    if (input instanceof Promise) {
-      return input
-    }
     let promise = cache.get(input)
     if (promise === undefined) {
       promise = new Promise((resolve, reject) => { input.then(resolve, reject) })
@@ -19,9 +19,10 @@ export function toPromise<T> (input: T | PromiseLike<T>): Promise<T> {
   }
   if (isObject(input)) {
     let promise = cache.get(input)
-    if (promise !== undefined) return promise
-    promise = Promise.resolve(input)
-    cache.set(input, promise)
+    if (promise === undefined) {
+      promise = Promise.resolve(input)
+      cache.set(input, promise)
+    }
     return promise
   }
   return Promise.resolve(input)
